861b57fd5cf33a8cbfcce67078509d3569c7e790
kiara <kiara.neporanne@gmail.com>
2026-09-07 18:18:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ from fastapi import FastAPI, HTTPException, status #Primero pongo de donde lo vo
                             #Para los codigos #status son los buenos
 app = FastAPI()
 
+@app.get("/productos")
+def listar_productos():
+    productos = session.query(Producto).all()
+    return productos
+
+@app.get("/productos/{id}")
+def obtener_producto(id: int):
+    producto = session.query(Producto).filter(Producto.id == id).first()
+    if producto is None:
+        raise HTTPException(status_code=404, detail="Persona no encontrada")
+    return producto
